ff23b1aebcc9e469b83035005f87437c9691c6b9
kperfetti <kperfetti@ideeli.com>
2014-05-29 17:45:48 -0400

    inital commit

---
+++ data_parser.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+FILES = [{:folder_name => '11206_2000', 
+          :data_file_name => 'DEC_00_SF1_DP1_with_ann.csv', 
+          :metadata_file_name => 'DEC_00_SF1_DP1_metadata.csv'}
+        ]
+
